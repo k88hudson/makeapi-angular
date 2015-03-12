28c944cc4868d46ad4932f45556f7e8345e34561
Scott <scott@localhost.localdomain>
2015-03-12 12:39:16 -0400

    adding a closure so module doesn't end up on window

--- dist/makeapi-angular.js
+++ dist/makeapi-angular.js
@@ -1,32 +1,95 @@
-var module = angular.module('wmMakeApiAngular', ['templates-makeApiAngular']);
-module.constant('Make', window.Make);
+(function() {
+  var module = angular.module('wmMakeApiAngular', ['templates-makeApiAngular']);
+  module.constant('Make', window.Make);
 
-module.provider('makeApi', function makeapiProvider() {
-  this.options = {
-    apiURL: 'https://makeapi.webmaker.org'
-  };
-  this.$get = ['$http', 'Make', function ($http, Make) {
-    var make = new Make(this.options);
-    make.get = function get() {
-      var querystring = this.queryPairs.join( "&" )
-      this.queryPairs = [];
-      return $http.get(this.apiURL + this.searchPath + '?' + querystring, { 'headers': {'X-CSRF-Token': undefined} });
+  module.provider('makeApi', function makeapiProvider() {
+    this.options = {
+      apiURL: 'https://makeapi.webmaker.org'
     };
-    return make;
-  }];
-});
+    this.$get = ['$http', 'Make', function ($http, Make) {
+      var make = new Make(this.options);
+      make.get = function get() {
+        var querystring = this.queryPairs.join( "&" )
+        this.queryPairs = [];
+        return $http.get(this.apiURL + this.searchPath + '?' + querystring, { 'headers': {'X-CSRF-Token': undefined} });
+      };
+      return make;
+    }];
+  });
 
-module.directive('make', [ 'makeApi',
-  function (makeApi) {
+  module.directive('make', [ 'makeApi',
+    function (makeApi) {
+      return {
+        restrict: 'EA',
+        replace: true,
+        scope: {
+          makeId: '@',
+          userId: '@',
+        },
+        templateUrl: 'make.html',
+        controller: ['$scope', '$http', '$rootScope', function($scope, $http, $rootScope) {
+          $scope.toggleLike = function() {
+            if (!$scope.liked) {
+              $http.post('/like', { makeID: $scope.make._id }).success(function(data, status, headers, config) {
+                $scope.liked = true;
+              }).error(function(data, status, headers, config) {
+                $scope.likeError = data;
+              });
+            } else {
+              $http.post('/unlike', { makeID: $scope.make._id }).success(function(data, status, headers, config) {
+                $scope.liked = false;
+              }).error(function(data, status, headers, config) {
+                $scope.likeError = data;
+              });
+            }
+          }
+
+
+          $scope.$watch('makeId', function (val) {
+            if (!val) {
+              return;
+            }
+            // Ignore ID if makeId is set
+            if ($scope.makeId && $scope.makeId !== val) {
+              return;
+            }
+            makeApi
+              .id(val)
+              .get()
+              .success(function (data) {
+                $scope.make = data.makes[0] || {};
+
+                $scope.liked = +$scope.make.likes.some(function(item){ return item.userId && item.userId == $scope.userId; });
+
+                $rootScope.$broadcast('makeLoad', { make: $scope.make });
+              });
+          });
+        }]
+      };
+  }]);
+
+  module.directive('makeData', function () {
     return {
-      restrict: 'EA',
+      restrict: 'A',
+      require: '^makeGallery',
       replace: true,
+      templateUrl: 'make.html',
       scope: {
-        makeId: '@',
+        makeData: '@',
         userId: '@',
       },
-      templateUrl: 'make.html',
       controller: ['$scope', '$http', '$rootScope', function($scope, $http, $rootScope) {
+        $scope.$watch('makeData', function (val) {
+          if (!val) {
+            return;
+          }
+
+          $scope.make = JSON.parse(val);
+          $scope.liked = +$scope.make.likes.some(function(item){ return item.userId && item.userId == $scope.userId; });
+
+          $rootScope.$broadcast('makeLoad', { make: $scope.make });
+        });
+
         $scope.toggleLike = function() {
           if (!$scope.liked) {
             $http.post('/like', { makeID: $scope.make._id }).success(function(data, status, headers, config) {
@@ -43,117 +106,56 @@ module.directive('make', [ 'makeApi',
           }
         }
 
+      }]
+    };
+  });
 
-        $scope.$watch('makeId', function (val) {
-          if (!val) {
-            return;
-          }
-          // Ignore ID if makeId is set
-          if ($scope.makeId && $scope.makeId !== val) {
-            return;
-          }
-          makeApi
-            .id(val)
-            .get()
-            .success(function (data) {
-              $scope.make = data.makes[0] || {};
-
-              $scope.liked = +$scope.make.likes.some(function(item){ return item.userId && item.userId == $scope.userId; });
-
-              $rootScope.$broadcast('makeLoad', { make: $scope.make });
+  module.directive('makeGallery', function () {
+      return {
+        restrict: 'EA',
+        transclude: true,
+        templateUrl: 'make-gallery.html',
+        scope: {
+          tags: '=',
+          user: '=',
+          ids: '=',
+          limit: '@',
+          makeList: '@getList',
+          containerClass: '@',
+          userId: '@',
+          csrfToken: '@',
+        },
+        controller: ['$scope', 'makeApi', function ($scope, makeApi) {
+          // Watch and update
+          ['tags', 'getList', 'ids', 'user'].forEach(function (attr) {
+            $scope.$watch(attr, function (val) {
+              if (!val) {
+                return;
+              }
+              makeApi[attr](val)
+                .limit($scope.limit || 20)
+                .get()
+                .success(function (data) {
+                  $scope.makes = data.makes;
+                });
             });
-        });
-      }]
-    };
-}]);
+          });
 
-module.directive('makeData', function () {
-  return {
-    restrict: 'A',
-    require: '^makeGallery',
-    replace: true,
-    templateUrl: 'make.html',
-    scope: {
-      makeData: '@',
-      userId: '@',
-    },
-    controller: ['$scope', '$http', '$rootScope', function($scope, $http, $rootScope) {
-      $scope.$watch('makeData', function (val) {
-        if (!val) {
-          return;
+        }]
+      };
+  });
+
+  module.directive('heart', ['makeApi', '$http',
+    function (makeApi, $http) {
+      return {
+        restrict: 'EA',
+        replace: true,
+        scope: {
+          liked: '@',
+        },
+        templateUrl: 'heart.html',
+        link: function (scope, element, attrs, ctrl) {
         }
-
-        $scope.make = JSON.parse(val);
-        $scope.liked = +$scope.make.likes.some(function(item){ return item.userId && item.userId == $scope.userId; });
-
-        $rootScope.$broadcast('makeLoad', { make: $scope.make });
-      });
-
-      $scope.toggleLike = function() {
-        if (!$scope.liked) {
-          $http.post('/like', { makeID: $scope.make._id }).success(function(data, status, headers, config) {
-            $scope.liked = true;
-          }).error(function(data, status, headers, config) {
-            $scope.likeError = data;
-          });
-        } else {
-          $http.post('/unlike', { makeID: $scope.make._id }).success(function(data, status, headers, config) {
-            $scope.liked = false;
-          }).error(function(data, status, headers, config) {
-            $scope.likeError = data;
-          });
-        }
-      }
-
-    }]
-  };
-});
-
-module.directive('makeGallery', function () {
-    return {
-      restrict: 'EA',
-      transclude: true,
-      templateUrl: 'make-gallery.html',
-      scope: {
-        tags: '=',
-        user: '=',
-        ids: '=',
-        limit: '@',
-        makeList: '@getList',
-        containerClass: '@',
-        userId: '@',
-        csrfToken: '@',
-      },
-      controller: ['$scope', 'makeApi', function ($scope, makeApi) {
-        // Watch and update
-        ['tags', 'getList', 'ids', 'user'].forEach(function (attr) {
-          $scope.$watch(attr, function (val) {
-            if (!val) {
-              return;
-            }
-            makeApi[attr](val)
-              .limit($scope.limit || 20)
-              .get()
-              .success(function (data) {
-                $scope.makes = data.makes;
-              });
-          });
-        });
-
-      }]
-    };
-});
-
-module.directive('heart', ['makeApi', '$http',
-  function (makeApi, $http) {
-    return {
-      restrict: 'EA',
-      replace: true,
-      scope: {
-        liked: '@',
-      },
-      templateUrl: 'heart.html',
-      link: function (scope, element, attrs, ctrl) {
-      }
-    };
-}]);
+      };
+  }]);
+}());
